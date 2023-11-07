0b4af35a70d32b73e892460e71626b834115ccb8
jairo-picott <jairo.picott@topbim.pt>
2023-11-07 16:25:50 +0000

    Update find_assets_with_same_sm

--- Level/find_assets_with_same_sm.py
+++ Level/find_assets_with_same_sm.py
@@ -8,8 +8,24 @@ editor_subsystem = unreal.UnrealEditorSubsystem()
 # Get the currently opened level
 current_level = editor_subsystem.get_editor_world()
 
-#Specify the name of the static mesh you are looking for
-static_mesh_name = "Doors_Vi-A2_Porta_Acustica_CF_VICAIMA_Portaro_EI30_AC_41dB_Guarnicao60mm__com_a"
+#initialize the static mesh actor variable
+static_mesh_name = ""
+
+#Get the static mesh of the selected actor
+selected_actor = level_library.get_selected_level_actors()
+count = len(selected_actor)
+
+
+if count == 1:
+    static_mesh = selected_actor[0].get_component_by_class(unreal.StaticMeshComponent)
+    if static_mesh and static_mesh.static_mesh:
+        static_mesh_name = static_mesh.static_mesh.get_name()
+    else:
+        print("ATENTION: The selected actor is not valid or does not have any StaticMeshComponent.")
+        exit()
+else:
+    print("ATENTION: Please select only one actor at a time.")
+    exit()
 
 
 
@@ -25,6 +41,8 @@ for actor in actors:
         if static_mesh_name == static_mesh_component.static_mesh.get_name():
             actors_with_sm.append(actor)
             #print(actor)
+        
+
 
 #Select the actors in the editor
 level_library.set_selected_level_actors(actors_with_sm)
