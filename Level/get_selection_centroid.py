7a4d0a1dd9f1abce86aa6c1fac2bc6a52d963907
jairo-picott <jairo.picott@topbim.pt>
2023-11-10 14:20:02 +0000

    Update get_selection_centroid.py

--- Level/get_selection_centroid.py
+++ Level/get_selection_centroid.py
@@ -2,28 +2,37 @@ import unreal
 
 #Import classes
 actor_subsystem = unreal.EditorActorSubsystem()
+gameplay_statics = unreal.GameplayStatics()
 
 #Get selected actors
 selected_actors = actor_subsystem.get_selected_level_actors()
 
-#Initialize min and max vectors with extreme values
+#Initialize min and max vectors with extreme values, and origin with 0 values
 min_vector = unreal.Vector(float('inf'), float('inf'), float('inf'))
 max_vector = unreal.Vector(float('-inf'), float('-inf'), float('-inf'))
 
+total_x = 0.0
+total_y = 0.0
+total_z = 0.0
+
 #Iterate over the actor to find the outermost points
 for actor in selected_actors:
-    actor_bound = actor.get_actor_bounds(False)
-    min_corner = actor_bound[0] - actor_bound[1]
-    max_corner = actor_bound[0] + actor_bound[1]
+
+    #Look for the DatasmithActor class
+    actor_location = actor.get_actor_location()
+
+    total_x = total_x + actor_location.x
+    total_y = total_y + actor_location.y
+    total_z = total_z + actor_location.z
 
     #Update the min and max vectors
-    min_vector.x = min(min_vector.x, min_corner.x)
-    min_vector.y = min(min_vector.y, min_corner.y)
-    min_vector.z = min(min_vector.z, min_corner.z)
+    min_vector.x = min(min_vector.x, actor_location.x)
+    min_vector.y = min(min_vector.y, actor_location.y)
+    min_vector.z = min(min_vector.z, actor_location.z)
 
-    max_vector.x = max(max_vector.x, max_corner.x)
-    max_vector.y = max(max_vector.y, max_corner.y)
-    max_vector.z = max(max_vector.z, max_corner.z)
+    max_vector.x = max(max_vector.x, actor_location.x)
+    max_vector.y = max(max_vector.y, actor_location.y)
+    max_vector.z = max(max_vector.z, actor_location.z)
 
 vertices = [
     unreal.Vector(min_vector.x, min_vector.y, min_vector.z),
@@ -36,15 +45,6 @@ vertices = [
     unreal.Vector(max_vector.x, max_vector.y, max_vector.z)
 ]
 
-for vertex in vertices:
-    #Update the min and max vectors
-    min_vector.x = min(min_vector.x, vertex.x)
-    min_vector.y = min(min_vector.y, vertex.y)
-    min_vector.z = min(min_vector.z, vertex.z)
-
-    max_vector.x = max(max_vector.x, vertex.x)
-    max_vector.y = max(max_vector.y, vertex.y)
-    max_vector.z = max(max_vector.z, vertex.z)
 
 # The dimensions of the box are the differences between the max and min vectors
 dimensions_x = max_vector.x - min_vector.x
@@ -63,5 +63,9 @@ origin_z = (min_vector.z + max_vector.z) / 2.0
 
 origin = unreal.Vector(origin_x, origin_y, origin_z)
 
+x = total_x / len(selected_actors)
+y = total_y / len(selected_actors)
+z = total_z / len(selected_actors)
+
 # Print the origin
-print("Origin of the bounding box is at: {}, {}, {}".format(origin.x, origin.y, origin.z))
+print("(X={},Y={},Z={})".format(x, y, z))
